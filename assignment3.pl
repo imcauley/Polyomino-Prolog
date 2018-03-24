7923902331e0f63593b0869740e1cf15e48b3d9f
imcauley <mcauleyisaac@gmail.com>
2018-03-24 12:31:20 -0600

    better output

--- assignment3.pl
+++ assignment3.pl
@@ -7,12 +7,19 @@ pieces([P1, P2]) :- piece1(P1), piece2(P2).
 % solve will take in a list of pieces and return a board where
 %  all pieces cannot be placed on the board
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% SOLUTON FINDER
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 solve([],B,B,_).
 solve([P|T],B,B3,(W,H)) :- numlist(0,W,Xlist),
                            numlist(0,H,Ylist),
                            member(X,Xlist),
                            member(Y,Ylist),
-                           translate(X,Y,P,PieceT),
+                           pieceCollection(P,PS),
+                           member(P1,PS),
+                           translate(X,Y,P1,PieceT),
                            inBounds(W,H,PieceT),
                            place(B,PieceT,B2),
                            solve(T, B2, B3, (W,H)).
@@ -28,7 +35,7 @@ inBounds(X,Y,[(_,PX,PY)|T]) :- PX >= 0, PY >= 0,
                                PX < X, PY < Y,
                                inBounds(X,Y,T).
 
-place(B,P,B2) :- fits(B,P) -> append(B,P,B2) ; B2 = B.
+place(B,P,B2) :- fits(B,P) -> append(B,P,B2) ; false.
 
 fits(_,[]).
 fits(B,[H|T]) :- notOnBoard(B,H), fits(B,T).
@@ -36,8 +43,43 @@ fits(B,[H|T]) :- notOnBoard(B,H), fits(B,T).
 notOnBoard([],_).
 notOnBoard([(_,BX,BY)|T], (P,PX,PY)) :- (BX \= PX ; BY \= PY), notOnBoard(T,(P,PX,PY)).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PIECE MANIPULATION
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+rotatePiece(P, P2) :- maplist(rotate, P, P2).
+rotate((A, X1, Y1), (A, X2, Y2)) :- X2 is Y1, Y2 is (-X1).
 
+flipPiece(P, P2) :- maplist(flip, P, P2).
+flip((A, X1, Y1), (A, X1, Y2)) :- Y2 is ((-Y1) + 1).
 
-makeList(0,[0]).
-makeList(M,[Q|L]) :- Q is M - 1, N is M - 2, makeList(N, L).
+pieceCollection(P, [P,PR,PRR,PRRR,
+                    PF,PFR,PFRR,PFRRR]) :- rotatePiece(P,PR),
+                                        rotatePiece(PR,PRR),
+                                        rotatePiece(PRR,PRRR),
+                                        flipPiece(P,PF),
+                                        flipPiece(PR,PFR),
+                                        flipPiece(PRR,PFRR),
+                                        flipPiece(PRRR,PFRRR).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% INPUT / OUTPUT
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+makeBoard(_,0,[]).
+makeBoard(N,M,[B|T]) :- makeString('#', N, B), M1 is M - 1, makeBoard(N, M1, T).
+
+makeString(_,0,[]).
+makeString(C,N,[C|T]) :- N1 is N - 1, makeString(C,N1,T).
+
+printBoard([]).
+printBoard([R|C]) :- printRow(R), printBoard(C).
+
+printRow([]) :- write('\n').
+printRow([C|R]) :- write(C), printRow(R).
+
+replaceInBoard(A,0,R,[H|T],[N|T]) :- replaceInRow(A,R,H,N).
+replaceInBoard(A,C,R,[H|T],[H|N]) :- C1 is C - 1, replaceInBoard(A,C1,R,T,N).
+
+replaceInRow(C,0,[_|T], [C|T]).
+replaceInRow(C,N,[H|T], [H|R]) :- M is N - 1, replaceInRow(C, M, T, R).
